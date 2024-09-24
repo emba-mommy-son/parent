583af7ec673d82e2ba6daa326c449842c84bc0f0
monam2 <kangcw0107@gmail.com>
2024-09-24 20:49:30 +0900

    initial commit

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,12 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['module:metro-react-native-babel-preset'],
-    plugins: ['nativewind/babel'],
+    presets: ['module:metro-react-native-babel-preset', '@babel/preset-typescript'],
+    plugins: [
+      'nativewind/babel',
+      ['@babel/plugin-transform-class-properties', { loose: true }],
+      ['@babel/plugin-transform-private-methods', { loose: true }],
+      ['@babel/plugin-transform-private-property-in-object', { loose: true }],
+    ],
   };
 };
