09f97df8900a36ccd3d01fd4d91df5b0e56d0255
monam2 <kangcw0107@gmail.com>
2024-09-25 00:47:10 +0900

    feat: 메인페이지 스택 라우트 설정

---
+++ src/app/home/HomeStack.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './HomeScreen';
+
+const Stack = createNativeStackNavigator();
+const HomeStack = () => {
+  return (
+    <Stack.Navigator initialRouteName="Home" screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
+    </Stack.Navigator>
+  );
+};
+
+export default HomeStack;
