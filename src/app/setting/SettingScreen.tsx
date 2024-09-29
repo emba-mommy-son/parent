8147c4bcb44654ef370119d305f20fdddc28ff41
monam2 <kangcw0107@gmail.com>
2024-09-30 04:13:11 +0900

    feat: 버튼 컴포넌트 테스트

--- src/app/setting/SettingScreen.tsx
+++ src/app/setting/SettingScreen.tsx
@@ -1,10 +1,22 @@
 import { View, Text } from 'react-native';
 import React from 'react';
+import Button from '../../components/common/buttons/Button';
 
 const SettingScreen = () => {
   return (
-    <View>
-      <Text className="text-primary">설정 페이지임 @@@@@@@@@@</Text>
+    <View className="w-full h-full items-center justify-center bg-slate-500">
+      <Text className="bg-red-500 text-lg w-[100%]">설정 페이지임 @@@@@@@@@@</Text>
+      <View className="w-full flex items-center">
+        {/* flex 속성 추가 */}
+        <Button
+          size="hug"
+          type="primary"
+          onPress={() => {
+            console.log('first');
+          }}>
+          버튼dfdfdf
+        </Button>
+      </View>
     </View>
   );
 };
