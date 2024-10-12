376559a0b70e6ebca94a1b22be7de975a736d7d2
monam2 <kangcw0107@gmail.com>
2024-10-12 21:43:11 +0900

    feat: 홈 스크린(테스트 중)

--- src/app/home/HomeScreen.tsx
+++ src/app/home/HomeScreen.tsx
@@ -12,16 +12,17 @@ type HomeScreenProp = BottomTabNavigationProp<RootTabParamList>;
 
 const HomeScreen = ({ navigation }: { navigation: HomeScreenProp }) => {
   const arr = Array.from({ length: 100 }, (_, index) => index);
-  const [val, setVal] = useState('');
+  const [val, setVal] = useState('여기는 텍스트입니다.');
 
   const handleDeleteText = () => {
     setVal('');
   };
 
   return (
-    <ScreenContainer>
-      <Input
-        className="items-center justify-center"
+    <ScreenContainer bgColor="white" barStyle="dark-content" barBgColor="black">
+      {/* <Input enableDeleteButton={true} handleDeleteText={handleDeleteText} value={val} onChangeText={setVal} /> */}
+      <LabeledInput
+        label="여기는 라벨입니다."
         enableDeleteButton={true}
         handleDeleteText={handleDeleteText}
         value={val}
