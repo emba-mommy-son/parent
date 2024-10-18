89e83c85fad84a7f06957ab542248849ab07b3c1
monam2 <kangcw0107@gmail.com>
2024-10-19 00:37:32 +0900

    feat: axios 인스턴스 생성

---
+++ src/configs/axios.ts
@@ -0,0 +1,69 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import axios from 'axios';
+
+import useRootStore from '@/zustand';
+
+const BASE_URL = 'https://www.mommy-son.kro.kr';
+
+const instance = axios.create({
+  baseURL: BASE_URL,
+  timeout: 2500,
+  withCredentials: true,
+  headers: {
+    'Content-Type': 'application/json',
+    Accept: 'application/json',
+  },
+});
+
+instance.interceptors.request.use(
+  config => {
+    // 요청 시점에 최신 상태 가져오기
+    const { accessToken } = useRootStore.getState();
+    if (accessToken) {
+      console.log('accessToken : ', accessToken);
+      config.headers['Authorization'] = `Bearer ${accessToken}`;
+    }
+    return config;
+  },
+  error => {
+    return Promise.reject(error);
+  },
+);
+
+instance.interceptors.response.use(
+  async response => {
+    console.log('성공');
+    return response;
+  },
+  async error => {
+    if (!error.response || error.response.status !== 401 || error.config._retry) {
+      return Promise.reject(error);
+    }
+
+    error.config._retry = true;
+
+    try {
+      // 최신 refreshToken 가져오기
+      const { refreshToken } = useRootStore.getState();
+      const refreshResponseData = await instance.post(`${BASE_URL}/api/auth/refresh`, { refreshToken });
+
+      const { accessToken: newAccessToken, refreshToken: newRefreshToken } = refreshResponseData.data;
+
+      // 새로운 토큰 저장 및 재요청
+      useRootStore.getState().setAccessToken(newAccessToken);
+      useRootStore.getState().setRefreshToken(newRefreshToken);
+      error.config.headers['Authorization'] = `Bearer ${newAccessToken}`;
+
+      return instance.request(error.config);
+    } catch (error: any) {
+      console.log('에러 발생', error.response.data.message);
+      await AsyncStorage.removeItem('accessToken');
+      await AsyncStorage.removeItem('refreshToken');
+      useRootStore.getState().clearToken();
+
+      return Promise.reject(error);
+    }
+  },
+);
+
+export default instance;
