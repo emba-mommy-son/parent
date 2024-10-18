210a45338127ca8285375efa56e4c62520f5c803
monam2 <kangcw0107@gmail.com>
2024-10-19 00:37:07 +0900

    fix: 린트 에러 수정

--- index.js
+++ index.js
@@ -2,8 +2,10 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+// eslint-disable-next-line import/namespace
+import { AppRegistry } from 'react-native';
+
+import { name as appName } from './app.json';
 import App from './src/App';
-import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
