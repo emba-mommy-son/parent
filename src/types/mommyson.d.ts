084d0ff178bea04e6f199400be89b144811bbedf
monam2 <kangcw0107@gmail.com>
2024-10-19 00:39:47 +0900

    rename: assets 파일 관련 타입 폴더 정리

--- src/types/mommyson.d.ts
+++
@@ -1,9 +0,0 @@
-interface MommysonResponse<T> {
-  code: number;
-  message: string;
-  data: T;
-}
-
-type AccessToken = string;
-type RefreshToken = string;
-type Role = 'PARENT' | 'CHILD'; // 부모, 자녀
