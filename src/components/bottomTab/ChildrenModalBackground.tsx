64622bd96540f96ff9e72f55c48e04fac0b8cb92
monam2 <kangcw0107@gmail.com>
2024-10-19 15:33:32 +0900

    feat: childModalSlice 상태 적용

---
+++ src/components/bottomTab/ChildrenModalBackground.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { TouchableOpacity } from 'react-native';
+
+import useRootStore from '@/zustand';
+
+const ChildrenModalBackground = () => {
+  const { isModalVisible, toggleModal } = useRootStore();
+  return (
+    <>
+      <TouchableOpacity
+        onPress={toggleModal}
+        className={`relative w-full h-full pb-10 bg-my_black opacity-40 z-0 ${
+          isModalVisible ? 'flex' : 'none'
+        }`}></TouchableOpacity>
+    </>
+  );
+};
+
+export default ChildrenModalBackground;
--- src/zustand/useChildModalStore.ts
+++
@@ -1,6 +0,0 @@
-import { create } from 'zustand';
-
-export const useChildModalStore = create<ChildModalStore>(set => ({
-  isModalVisible: false,
-  toggleModal: () => set(state => ({ isModalVisible: !state.isModalVisible })),
-}));
