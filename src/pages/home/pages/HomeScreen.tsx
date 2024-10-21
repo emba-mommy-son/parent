c26853adef892f3e93c79cb940672a03066a842e
monam2 <kangcw0107@gmail.com>
2024-10-21 19:58:08 +0900

    feat: 메인 페이지 스타일 수정

--- src/pages/home/pages/HomeScreen.tsx
+++ src/pages/home/pages/HomeScreen.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import { View } from 'react-native';
+
 import ScreenContainer from '@/components/ScreenContainer';
+
 import ChildStatusCard from '../components/ChildStatusCard';
-import LocationCard from '../components/LocationCard';
 import EmotionStatsCard from '../components/EmotionStatsCard';
+import LocationCard from '../components/LocationCard';
 import ProfileCard from '../components/ProfileCard';
 
 const HomeScreen = () => {
   return (
-    <ScreenContainer isPadding={false}>
+    <ScreenContainer bgColor="white" barStyle="dark-content" barBgColor="black" isPadding={false}>
       <View className="relative">
         <ProfileCard />
         <View className="absolute z-10 top-[90px]">
