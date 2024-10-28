8ff9d33daa6fbeb6e638575490916cb769676f77
monam2 <kangcw0107@gmail.com>
2024-10-28 21:37:38 +0900

    feat: 기본 타입 추가

--- src/types/common.d.ts
+++ src/types/common.d.ts
@@ -1,4 +1,12 @@
 /**
+ * 기본 타입
+ */
+type Day = number;
+type month = number;
+type year = number;
+type DateString = string;
+
+/**
  * 토큰
  */
 type Token = string;
@@ -7,7 +15,11 @@ type RefreshToken = Token;
 type NewAccessToken = Token;
 type NewRefreshToken = Token;
 
-//유저
+/**
+ * 자녀
+ */
+type ChildId = number;
+
 type UserId = string;
 /**
  * 유저 아이디(이메일)
@@ -16,8 +28,6 @@ type UserName = string;
 type Name = string;
 type PhoneNumber = string;
 type Password = string;
-
-type DateString = string;
 
 /**
  * 자녀 위치 위경도 타입
