32803d544033c0c64b532f766eb0f51a242076da
monam2 <kangcw0107@gmail.com>
2024-10-29 01:21:11 +0900

    style: import문 순서 변경

--- src/components/chart/EmotionChart.tsx
+++ src/components/chart/EmotionChart.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import AntDesign from 'react-native-vector-icons/AntDesign';
 
 interface EmotionChartProps {
