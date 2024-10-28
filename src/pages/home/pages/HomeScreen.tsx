f96b80d9b1c1e3cf2c954cc4117419111d2be802
monam2 <kangcw0107@gmail.com>
2024-10-29 03:23:08 +0900

    feat: 임시 자녀 데이터 추가

--- src/pages/home/pages/HomeScreen.tsx
+++ src/pages/home/pages/HomeScreen.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View } from 'react-native';
 
 import ScreenContainer from '@/components/ScreenContainer';
+import useRootStore from '@/zustand';
 
 import ChildStatusCard from '../components/ChildStatusCard';
 import EmotionSummary from '../components/EmotionSummary';
@@ -9,12 +10,26 @@ import LocationCard from '../components/LocationCard';
 import ProfileCard from '../components/ProfileCard';
 
 const HomeScreen = () => {
+  const { nowSelectedChild, setNowSelectedChild } = useRootStore();
+
+  useEffect(() => {
+    // api 연결(선택된 자녀 정보 불러오기)
+
+    //임시
+    setNowSelectedChild({
+      childId: 1,
+      name: '김도영김도올드',
+      profileImage:
+        'https://i.namu.wiki/i/38Gd7TqELmVjZOxv_o9IlD9z5RVQ33-FSCzQS-AFY6COMYpxFcH_XjAALWm_Gswh9xwBUVENkwgwGwnj8a-cAg.webp',
+    });
+  }, []);
+
   return (
     <ScreenContainer bgColor="white" barBgColor="black" isPadding={false}>
       <View className="relative">
-        <ProfileCard />
+        <ProfileCard nowSelectedChild={nowSelectedChild} />
         <View className="absolute z-10 top-[90px]">
-          <ChildStatusCard />
+          <ChildStatusCard nowSelectedChild={nowSelectedChild} />
           <LocationCard />
           <EmotionSummary />
         </View>
