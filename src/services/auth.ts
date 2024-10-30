7bde3605370cf38bdda07b2b072f52c2e497514b
monam2 <kangcw0107@gmail.com>
2024-10-31 03:25:13 +0900

    feat: 자녀 회원가입 api 호출 함수 정의

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -5,27 +5,32 @@ export default {
    * 회원 가입
    */
   signUp: async (signupForm: SignUpForm) => {
-    return axios.post<BaseResponse<null>>('/api/v1/auth/sign-up/parent', signupForm);
+    return axios.post<BaseResponse<void>>('/api/v1/auth/sign-up/parent', signupForm);
   },
 
   /**
    * 로그인
    */
   signIn: async (signInForm: SignInForm) => {
-    return axios.post<BaseResponse<GetSignInResponseDto>>('/api/v1/auth/sign-in', signInForm);
+    return axios.post<BaseResponse<SignInResponseDto>>('/api/v1/auth/sign-in', signInForm);
+  },
+
+  /** 자녀 회원가입 */
+  signUpChild: async (signUpChildForm: SignUpChildForm) => {
+    return axios.post<BaseResponse<void>>('/api/v1/auth/sign-up/child', signUpChildForm);
   },
 
   /**
    * 토큰 리프레시
    */
   refreshToken: async (refreshToken: RefreshToken) => {
-    return axios.post<BaseResponse<GetRefreshTokenResponseDto>>('/api/v1/auth/refresh', { refreshToken });
+    return axios.post<BaseResponse<RefreshTokenResponseDto>>('/api/v1/auth/refresh', { refreshToken });
   },
 
   /**
    * 부모-자녀 리커넥트
    */
   reconnect: async ({ fcmToken, childId }: ReconnectForm) => {
-    return axios.post<BaseResponse<null>>('/api/v1/auth/reconnect', { fcmToken, childId });
+    return axios.post<BaseResponse<void>>('/api/v1/auth/reconnect', { fcmToken, childId });
   },
 };
