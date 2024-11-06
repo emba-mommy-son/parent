8a24c2adb21ed14e7a4db09d948ecb129c118221
monam2 <kangcw0107@gmail.com>
2024-11-07 03:33:42 +0900

    feat: 감정 분석 컴포넌트 데이터 바인딩

--- src/components/chart/EmotionChart.tsx
+++ src/components/chart/EmotionChart.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
 import { useNavigation } from '@react-navigation/native';
 import { type NativeStackNavigationProp } from '@react-navigation/native-stack';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import AntDesign from 'react-native-vector-icons/AntDesign';
 import Feather from 'react-native-vector-icons/Feather';
 
+import { useChildEachEmotionReport } from '@/tanstackQuery/queries/analysis';
 import { RootStackParamList } from '@/types/navigation';
+import useRootStore from '@/zustand';
 
 interface EmotionChartProps {
   showReport?: boolean;
@@ -13,15 +15,67 @@ interface EmotionChartProps {
 
 const EmotionChart: React.FC<EmotionChartProps> = ({ showReport = true }) => {
   const navigation = useNavigation<NativeStackNavigationProp<RootStackParamList>>();
-
-  const data = [
+  const { nowSelectedChild, selectedDate } = useRootStore();
+  const eachEmotionData = useChildEachEmotionReport({ childId: nowSelectedChild?.id ?? 0, dateTime: selectedDate });
+  const [isData, setIsData] = useState(false);
+  const [data, setData] = useState([
     { emotion: '즐거움', score: 48, reliability: 0.5 },
     { emotion: '놀라움', score: 36, reliability: 0.8 },
     { emotion: '공포', score: 17, reliability: 0.4 },
     { emotion: '슬픔', score: 15, reliability: 0.7 },
     { emotion: '분노', score: 46, reliability: 0.5 },
     { emotion: '혐오', score: 7, reliability: 0.2 },
-  ];
+    // { emotion: '즐거움', score: 0, reliability: 0 },
+    // { emotion: '놀라움', score: 0, reliability: 0 },
+    // { emotion: '공포', score: 0, reliability: 0 },
+    // { emotion: '슬픔', score: 0, reliability: 0 },
+    // { emotion: '분노', score: 0, reliability: 0 },
+    // { emotion: '혐오', score: 0, reliability: 0 },
+  ]);
+
+  const calculateReliability = (emotionCount: number, totalMessages: number) => {
+    return totalMessages > 0 ? emotionCount / totalMessages : 0;
+  };
+
+  useEffect(() => {
+    if (eachEmotionData) {
+      setIsData(true);
+      setData([
+        {
+          emotion: '즐거움',
+          score: eachEmotionData.delightPercentage,
+          reliability: calculateReliability(eachEmotionData.delightCount, eachEmotionData.totalAnalyzedMessages),
+        },
+        {
+          emotion: '놀라움',
+          score: eachEmotionData.surprisePercentage,
+          reliability: calculateReliability(eachEmotionData.surpriseCount, eachEmotionData.totalAnalyzedMessages),
+        },
+        {
+          emotion: '공포',
+          score: eachEmotionData.horrorPercentage,
+          reliability: calculateReliability(eachEmotionData.horrorCount, eachEmotionData.totalAnalyzedMessages),
+        },
+        {
+          emotion: '슬픔',
+          score: eachEmotionData.sorrowPercentage,
+          reliability: calculateReliability(eachEmotionData.sorrowCount, eachEmotionData.totalAnalyzedMessages),
+        },
+        {
+          emotion: '분노',
+          score: eachEmotionData.angerPercentage,
+          reliability: calculateReliability(eachEmotionData.angerCount, eachEmotionData.totalAnalyzedMessages),
+        },
+        {
+          emotion: '혐오',
+          score: eachEmotionData.aversionPercentage,
+          reliability: calculateReliability(eachEmotionData.aversionCount, eachEmotionData.totalAnalyzedMessages),
+        },
+      ]);
+    } else {
+      setIsData(false);
+    }
+  }, [eachEmotionData]);
 
   const textScale = ['#4CAF50', '#FF9923', '#9C27B0', '#3F88AD', '#FF749E', '#6B6B6B'];
   const bgScale = ['#DFF3E4', '#FFF2CC', '#EAD8FA', '#CCE3FF', '#FFD9E1', '#E6E6E6'];
@@ -59,6 +113,13 @@ const EmotionChart: React.FC<EmotionChartProps> = ({ showReport = true }) => {
           <View className="flex flex-row items-center justify-center gap-1 w-full pt-2 pb-1">
             <Feather name="info" size={14} style={{ color: '#cacaca' }} />
             <Text className="mb-1 mr-0.5 text-[#aaaaaa] text-xs">백분율은 정보 신뢰도를 의미합니다.</Text>
+          </View>
+        )}
+        {!isData && (
+          <View
+            className="absolute top-0 w-full h-full justify-center rounded-lg items-center"
+            style={{ backgroundColor: 'rgba(24, 24, 24, 0.8)' }}>
+            <Text className="text-white text-lg">자녀의 감정 기록이 없습니다.</Text>
           </View>
         )}
       </View>
