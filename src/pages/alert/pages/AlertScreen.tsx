d060ce20aee6419a7e723da90145e6164c1711d5
monam2 <kangcw0107@gmail.com>
2024-11-09 15:52:50 +0900

    feat: 알림 페이지 훅 연동

--- src/pages/alert/pages/AlertScreen.tsx
+++ src/pages/alert/pages/AlertScreen.tsx
@@ -1,58 +1,67 @@
+import { useQueryClient } from '@tanstack/react-query';
 import React from 'react';
 import { ScrollView, StyleSheet, Text, View } from 'react-native';
 import Feather from 'react-native-vector-icons/Feather';
 
+import Button from '@/components/buttons/Button';
 import ScreenContainer from '@/components/ScreenContainer';
+import { keys } from '@/tanstackQuery/keys';
+import { useGetNotification, usePostNotifications } from '@/tanstackQuery/queries/notification';
 
 // 현재 코드는 더미데이터 출력용임. 넘어오는 데이터 구조에 따라 로직을 변경할 필요가 있음.
 const AlertScreen = () => {
-  const noAlert = false;
+  // 알림 목록 조회
+  const notificationList: notificationDto[] | undefined = useGetNotification();
 
-  type AlertData = {
-    [date: string]: string[];
+  //알림 읽음 처리 뮤테이션
+  const postNotification = usePostNotifications();
+  const queryClient = useQueryClient();
+
+  const readAllNoti = () => {
+    postNotification.mutate(undefined, {
+      onSuccess: () => {
+        // 모두 읽었으면 키 무효화해서 다시 로드
+        queryClient.invalidateQueries({ queryKey: keys.getNotification() });
+      },
+    });
   };
 
-  const lastAccessTime = '';
-  const data: AlertData = {
-    '2024.10.26': ['스트레스가 3일간 지속되었습니다', '자녀가 안전구역을 벗어났습니다'],
-    '2024.10.25': [
-      '자녀가 학교에서 나왔습니다',
-      '자녀가 학교에 도착했습니다',
-      '자녀가 집에서 나왔습니다',
-      '자녀가 학교에 도착했습니다',
-      '자녀가 집에서 나왔습니다',
-    ],
+  const createdAtSlicer = (day: string) => {
+    return day.split('T')[0].split('-').splice(2).join('-');
   };
 
   return (
-    <ScreenContainer barStyle="dark-content">
-      {noAlert ? (
+    <ScreenContainer barStyle="dark-content" ContainerStyle={{ justifyContent: 'space-between' }}>
+      {notificationList && notificationList.length === 0 ? (
         <View className="bg-[#F4F4F4] flex flex-column justify-center items-center pt-4 pb-5 gap-1.5">
           <Feather name="info" size={18} color={'#9c9c9c'} />
           <Text>도착한 알림이 없습니다.</Text>
         </View>
       ) : (
         <ScrollView className="px-4 pb-2.5">
-          {Object.keys(data).map((date, index) => (
-            <View key={index} className="mb-5">
-              <Text className="text-center text-gray-500 mb-2">{date}</Text>
-              {data[date].map((message, msgIndex) => (
-                <View
-                  key={msgIndex}
-                  className="flex flex-row justify-between items-center py-3"
-                  style={styles.alertContainer}>
-                  <Text style={msgIndex < 2 ? styles.newMessage : styles.oldMessage}>{message}</Text>
-                  {msgIndex < 2 && (
-                    <View className="ml-2 bg-my_primary rounded-full w-5 h-5 flex items-center justify-center">
-                      <Text className="text-white text-xs font-bold">N</Text>
-                    </View>
-                  )}
+          <View className="mb-5">
+            {notificationList &&
+              notificationList.map(notidata => (
+                <View className="flex flex-row justify-between items-center py-3" style={styles.alertContainer}>
+                  <View className="flex-row">
+                    <Text style={!notidata.read ? styles.newMessage : styles.oldMessage}>{notidata.message}</Text>
+                    {!notidata.read && (
+                      <View className="ml-2 bg-my_primary rounded-full w-5 h-5 flex items-center justify-center">
+                        <Text className="text-white text-xs font-bold">N</Text>
+                      </View>
+                    )}
+                  </View>
+                  <Text>{createdAtSlicer(notidata.createdAt)}</Text>
                 </View>
               ))}
-            </View>
-          ))}
+          </View>
         </ScrollView>
       )}
+      <View>
+        <Button onPress={readAllNoti} myTextStyle="text-white">
+          모두 읽기
+        </Button>
+      </View>
     </ScreenContainer>
   );
 };
